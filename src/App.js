bac3488a370c4131f562e608f8bd5fddf448d99d
Rohit Prasad <pdrohit674@gmail.com>
2022-10-16 20:19:32 +0530

    added data to utils

--- src/App.js
+++ src/App.js
@@ -2,47 +2,8 @@ import React from 'react'
 import './style.css'
 import { Navbar, Card, Footer, Form } from './components'
 import bannerImg from './assets/1.png'
-import invoice1 from './assets/invoice1.svg'
-import invoice2 from './assets/invoice2.svg'
-import invoice3 from './assets/invoice3.svg'
-import invoice4 from './assets/invoice4.svg'
-import invoice5 from './assets/invoice5.svg'
-import invoice6 from './assets/invoice6.svg'
+import { cardDetails } from './utils/data'
 export default function App() {
-  const cardDetails = [
-    {
-      cardImage: invoice1,
-      cardHeading: 'Quick Verification and Cash Flow',
-      cardParagraph:
-        'Verify your business instantly on the KredX platform and get funds in 24-72 hours*',
-    },
-    {
-      cardImage: invoice2,
-      cardHeading: 'Risk-free Assets',
-      cardParagraph: `Get cash against your company's unpaid invoices, No need to pledge any assets`,
-    },
-    {
-      cardImage: invoice3,
-      cardHeading: 'Avoid New Debt',
-      cardParagraph: `Grow your business without impacting your balance sheet`,
-    },
-    {
-      cardImage: invoice4,
-      cardHeading: 'Real-time Updates',
-      cardParagraph: `Track your invoices status on the go through KredX self-serve module`,
-    },
-    {
-      cardImage: invoice5,
-      cardHeading: 'Paperless Process',
-      cardParagraph: `Upload your documents and invoices easily on the KredX platform`,
-    },
-    {
-      cardImage: invoice6,
-      cardHeading: 'End-to-end Management',
-      cardParagraph: `Smooth and transparent process from invoice listing to funds disbursal`,
-    },
-  ]
-
   return (
     <div>
       <Navbar></Navbar>
